b4ae2609edd2aa18d0fa9ff0cf42d2a139232ec3
GuilhermeHigino05 <guihiginoo@gmail.com>
2026-05-10 16:56:29 -0300

    inicializando a criação do carrinho

---
+++ src/public/js/cartModal.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let listaCarrinho = [];
+    let carrinho = localStorage.getItem('carrinho');
+    if(carrinho) listaCarrinho = JSON.parse(carrinho);
+
+    function addToCart(){
+        
+    }
+})
